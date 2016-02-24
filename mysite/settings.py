9772219b8aad00b7729c592793b41aa3aa120418
wbubicz <wojciech.bubicz@gmail.com>
2016-02-24 14:48:44 +0000

    Dziala na pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
 		'NAME': 'wbubicz$djangogirls',
 		'USER': 'wbubicz',
 		'PASSWORD': 'pamysqlpass',
-		'HOST': 'localhost',
-		'PORT': '',
+		'HOST': 'mysql.server',
+		'PORT': '3306',
 	}
 }
 
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join('/', 'static')
