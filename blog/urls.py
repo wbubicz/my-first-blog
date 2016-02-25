f46b6418833565c644938328eb23f098fdfbaebb
wbubicz <wojciech.bubicz@gmail.com>
2016-02-25 16:47:04 +0100

    Modyfikacja szablonu, by pokazywal posty z bazy danych

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.nazwa_metody_renderujacej_dany_widok, name='post_list'),
 ]
