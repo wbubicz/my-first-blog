af7da7bfebc3256dc862b55e400bd7e72772d068
wbubicz <wojciech.bubicz@gmail.com>
2016-03-02 15:02:52 +0100

    Dodano widok tworzenia/edytowania posta bezposrednio na stronie.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.nazwa_metody_renderujacej_dany_widok, name='post_list'),
 	url(r'^post/(?P<pobrany_parametr>[0-9]+)/$', views.renderuj_widok_z_parametrem, name='nazwa_urla'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def nazwa_metody_renderujacej_dany_widok(request):
@@ -11,3 +12,32 @@ def nazwa_metody_renderujacej_dany_widok(request):
 def renderuj_widok_z_parametrem(request, pobrany_parametr):
 	post = get_object_or_404(Post, id=pobrany_parametr)
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('nazwa_urla', pobrany_parametr=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('nazwa_urla', pobrany_parametr=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
