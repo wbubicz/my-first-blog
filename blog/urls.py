59f8ed521c9547014622e8081fbfefd480742017
wbubicz <wojciech.bubicz@gmail.com>
2016-03-02 13:15:50 +0100

    Szczegółowy widok i generowane urle

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.nazwa_metody_renderujacej_dany_widok, name='post_list'),
+	url(r'^post/(?P<pobrany_parametr>[0-9]+)/$', views.renderuj_widok_z_parametrem, name='nazwa_urla'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +6,8 @@ from .models import Post
 def nazwa_metody_renderujacej_dany_widok(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def renderuj_widok_z_parametrem(request, pobrany_parametr):
+	post = get_object_or_404(Post, id=pobrany_parametr)
+	return render(request, 'blog/post_detail.html', {'post': post})
